116bd6f21e959e76b020828412c0aa73ed86390b
julio-silveira <julioqueiroz15@gmail.com>
2023-07-14 09:02:33 -0300

    chore: configuration for alembic

--- server/app/db/init_db.py
+++ server/app/db/init_db.py
@@ -3,7 +3,7 @@ import logging
 from sqlalchemy.orm import Session
 
 # from server import schemas
-from server.db import base  # noqa: F401
+from app.db import base  # noqa: F401
 # from server.core.settings import settings
 
 logger = logging.getLogger(__name__)
