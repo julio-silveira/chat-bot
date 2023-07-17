10904146e12a15aee86e642187a016d62c214d2c
julio-silveira <julioqueiroz15@gmail.com>
2023-07-17 08:07:30 -0300

    chore: setup of axios and react query for requests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,24 @@
 import { Container, ThemeProvider } from '@mui/material'
 import { RouterProvider } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
+import { QueryClientProvider } from '@tanstack/react-query';
+
 import router from '@/routes'
 import theme from '@/theme'
+import { queryClient } from '@/services'
 
 import 'react-toastify/dist/ReactToastify.css';
-
 
 function App() {
 
   return (
     <Container>
-      <ThemeProvider theme={theme}>
-        <RouterProvider  router={router}/>
-        <ToastContainer />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={router}/>
+          <ToastContainer />
+        </ThemeProvider>
+      </QueryClientProvider>
     </Container>
 
   )
