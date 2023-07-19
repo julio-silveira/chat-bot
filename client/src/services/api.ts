6c4fbe8165cedface6e09d65814d64920172bebf
julio-silveira <julioqueiroz15@gmail.com>
2023-07-19 16:21:31 -0300

    hardcoded api url

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const API_URL = 'https://timely-rain-production.up.railway.app/'
 
 const api = axios.create({
   baseURL: API_URL
